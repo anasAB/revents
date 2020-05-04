5c7cb335e6bb776e6b9f8a7df9777fbbd23f2584
anasAB <anas.222@hotmail.com>
2020-05-04 15:46:49 +0200

    open Close Form

--- src/features/event/EeventForm/EventForm.jsx
+++ src/features/event/EeventForm/EventForm.jsx
@@ -29,7 +29,9 @@ class EventForm extends Component {
           <Button positive type="submit">
             Submit
           </Button>
-          <Button type="button">Cancel</Button>
+          <Button type="button" onClick={this.props.cancelFromOpen}>
+            Cancel
+          </Button>
         </Form>
       </Segment>
     );
