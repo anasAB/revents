34ad63c9528ef796b463b7580c22408c4683fbab
anasAB <anas.222@hotmail.com>
2020-05-07 13:52:18 +0200

    add red-example

--- src/features/testarea/testActions.jsx
+++ src/features/testarea/testActions.jsx
@@ -1,15 +1,12 @@
 import { INCREMENT_COUNTER, DECREMENT_COUNTER } from "./testConstance";
 
 export const incrementCounter = () => {
-  console.log("Action incrementCounter");
   return {
     type: INCREMENT_COUNTER,
   };
 };
 
 export const decrementCounter = () => {
-  console.log("Action decrementCounter");
-
   return {
     type: DECREMENT_COUNTER,
   };
