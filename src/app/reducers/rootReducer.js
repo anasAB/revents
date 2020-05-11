d11b79833a326e5c3abbec596bd4997d8f572751
anasAB <anas.222@hotmail.com>
2020-05-11 08:42:13 +0200

    fixed bug

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -2,9 +2,9 @@ import { combineReducers } from "redux";
 import eventReducer from "../../features/event/eventReducer";
 import testReducer from "../../features/testarea/testReducer";
 
-const rootReducer = combineReducers({
+const RootReducer = combineReducers({
   tests: testReducer,
   events: eventReducer,
 });
 
-export default rootReducer;
+export default RootReducer;
--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,9 +1,9 @@
 import { createStore } from "redux";
 import { devToolsEnhancer } from "redux-devtools-extension";
-import rootReducer from "../Reducers/rootReducers";
+import RootReducer from "../reducers/rootReducer";
 
 export const configureStore = () => {
-  const store = createStore(rootReducer, devToolsEnhancer());
+  const store = createStore(RootReducer, devToolsEnhancer());
 
   return store;
 };
